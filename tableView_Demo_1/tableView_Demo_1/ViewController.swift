8d97321f7f69f2f13e679bbc4d7539016a5aed9a
Third Rock Techkno <trt-ios-04@Thirds-MacBook-Air.local>
2020-04-16 11:17:58 +0530

    TableView added in view Controller

---
+++ tableView_Demo_1/tableView_Demo_1/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  tableView_Demo_1
+//
+//  Created by Third Rock Techkno on 16/04/20.
+//  Copyright © 2020 Third Rock Techkno. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+        
+    }
+
+
+}
+
---
+++ tableView_Demo_1/tableView_Demo_1Tests/tableView_Demo_1Tests.swift
@@ -0,0 +1,34 @@
+//
+//  tableView_Demo_1Tests.swift
+//  tableView_Demo_1Tests
+//
+//  Created by Third Rock Techkno on 16/04/20.
+//  Copyright © 2020 Third Rock Techkno. All rights reserved.
+//
+
+import XCTest
+@testable import tableView_Demo_1
+
+class tableView_Demo_1Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
